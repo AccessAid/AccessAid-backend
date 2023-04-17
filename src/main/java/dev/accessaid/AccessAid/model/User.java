ba670c5d06105429031bb5f0660ad6dd2a3a7bde
Vero Conesa <vero.gconesa@gmail.com>
2023-04-17 10:50:05 +0200

    Added user Entity (for DB connection)

--- src/main/java/dev/accessaid/AccessAid/model/User.java
+++ src/main/java/dev/accessaid/AccessAid/model/User.java
@@ -1,5 +1,29 @@
 package dev.accessaid.AccessAid.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
 
 }
