cb06c7a70a03b1e4e2396fa6bde0bd03ad4124db
Vero Conesa <vero.gconesa@gmail.com>
2023-04-20 16:40:44 +0200

    Added ErrorResponse

---
+++ src/main/java/dev/accessaid/AccessAid/Geolocation/Response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package dev.accessaid.AccessAid.Geolocation.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+
+}
