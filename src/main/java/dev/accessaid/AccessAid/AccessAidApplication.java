324aae58d50021de53e04336eb5bae56d40c01ec
Cristobal Rosa <crisrogar@gmail.com>
2023-04-21 13:32:11 +0200

    feat:Actualizacion UserController

--- src/main/java/dev/accessaid/AccessAid/AccessAidApplication.java
+++ src/main/java/dev/accessaid/AccessAid/AccessAidApplication.java
@@ -2,10 +2,11 @@ package dev.accessaid.AccessAid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AccessAidApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccessAidApplication.class, args);
 	}
--- src/main/java/dev/accessaid/AccessAid/controller/UserController.java
+++ src/main/java/dev/accessaid/AccessAid/controller/UserController.java
@@ -1,5 +1,12 @@
 package dev.accessaid.AccessAid.controller;
 
+import dev.accessaid.AccessAid.security.payload.MessageResponse;
+import dev.accessaid.AccessAid.security.payload.RegisterRequest;
+import dev.accessaid.AccessAid.security.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<MessageResponse> registerUser(@Valid @RequestBody RegisterRequest signUpRequest) {
+        return userService.registerUser(signUpRequest);
+    }
 }
