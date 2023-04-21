b2e3a575220a5a89b68e0acb49151e1c852f02d7
Cristobal Rosa <crisrogar@gmail.com>
2023-04-21 08:57:11 +0200

    feat:metodos UserRepository

--- src/main/java/dev/accessaid/AccessAid/repository/UserRepository.java
+++ src/main/java/dev/accessaid/AccessAid/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package dev.accessaid.AccessAid.repository;
 
+import dev.accessaid.AccessAid.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
