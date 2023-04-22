8e668e13182fff64948066e87bae7728f36599cc
Vero Conesa <vero.gconesa@gmail.com>
2023-04-22 17:56:37 +0200

    Created placecontroller file

---
+++ src/main/java/dev/accessaid/AccessAid/Places/controller/PlaceController.java
@@ -0,0 +1,34 @@
+package dev.accessaid.AccessAid.Places.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.accessaid.AccessAid.Places.model.Place;
+
+@RestController
+public class PlaceController {
+
+    @GetMapping("/places")
+    public String getPlaces() {
+        return "Place";
+    }
+
+    @PostMapping("/places")
+    public String createPlace(@RequestBody Place place) {
+        return "Place";
+    }
+
+    @PutMapping("/places/{id}")
+    public String updatePlace(@RequestBody Place place) {
+        return "Place";
+    }
+
+    @DeleteMapping("/places/{id}")
+    public String deletePlace(@RequestBody Place place) {
+        return "Place";
+    }
+}
