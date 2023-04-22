46aa729442001070b7ddfd20b440c0337821f18d
Vero Conesa <vero.gconesa@gmail.com>
2023-04-22 13:35:04 +0200

    Added rating validation (min:1, max:5, notNull)

--- src/main/java/dev/accessaid/AccessAid/Ratings/model/Rating.java
+++ src/main/java/dev/accessaid/AccessAid/Ratings/model/Rating.java
@@ -8,6 +8,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,6 +27,10 @@ public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
     Double rating;
     Integer user_id;
 
