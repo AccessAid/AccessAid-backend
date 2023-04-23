a878b870d87adfe0ac34cfc9bcff08dd9f39b0e5
Vero Conesa <vero.gconesa@gmail.com>
2023-04-23 19:10:44 +0200

    Created CommentResponse

---
+++ src/main/java/dev/accessaid/AccessAid/Comments/Response/CommentResponse.java
@@ -0,0 +1,26 @@
+package dev.accessaid.AccessAid.Comments.Response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentResponse {
+
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("comment")
+    private String comment;
+
+    @JsonProperty("user_id")
+    private Integer user_id;
+
+    @JsonProperty("place_id")
+    private Integer place_id;
+
+}
