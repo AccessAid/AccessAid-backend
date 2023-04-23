461d4b65fba83b2ac3996b41d21edf971a9370a4
Vero Conesa <vero.gconesa@gmail.com>
2023-04-23 21:46:29 +0200

    Created findUsers and CommentsByid in PlaceRepo

--- src/main/java/dev/accessaid/AccessAid/Places/repository/PlaceRepository.java
+++ src/main/java/dev/accessaid/AccessAid/Places/repository/PlaceRepository.java
@@ -1,13 +1,27 @@
 package dev.accessaid.AccessAid.Places.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import dev.accessaid.AccessAid.Comments.model.Comment;
 import dev.accessaid.AccessAid.Places.model.Place;
+import dev.accessaid.AccessAid.model.User;
 
 @Repository
-public class PlaceRepository {
+public interface PlaceRepository extends JpaRepository<Place, Integer> {
 
-    public void save(Place newPlace) {
-    }
+    // List<Place> findAllPlaces();
+
+    // Place findPlaceById(Integer id);
+
+    // <S extends Place> S save(S place);
+
+    // Place deletePlaceById(Integer id);
+
+    List<User> findUsersByid(Integer id);
+
+    List<Comment> findCommentsByid(Integer id);
 
 }
