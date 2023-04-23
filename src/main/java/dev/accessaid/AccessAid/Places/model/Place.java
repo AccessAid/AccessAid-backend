1dbbf171c9524dcaff8346828813de57b1bad6b5
Vero Conesa <vero.gconesa@gmail.com>
2023-04-23 21:43:48 +0200

    Added GeolocationResponse to get info from API

--- src/main/java/dev/accessaid/AccessAid/Places/model/Place.java
+++ src/main/java/dev/accessaid/AccessAid/Places/model/Place.java
@@ -1,5 +1,6 @@
 package dev.accessaid.AccessAid.Places.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -7,11 +8,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.accessaid.AccessAid.Comments.model.Comment;
 import dev.accessaid.AccessAid.Geolocation.Response.GeolocationResponse;
 import dev.accessaid.AccessAid.Ratings.model.Rating;
+import dev.accessaid.AccessAid.model.User;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -38,7 +43,10 @@ public class Place {
     @NotNull
     double longitude;
 
+    @NotNull
     String address;
+
+    @NotNull
     String api_placeId;
 
     @JsonIgnore
@@ -51,11 +59,20 @@ public class Place {
     @OneToMany(mappedBy = "place", cascade = CascadeType.ALL)
     List<Comment> comments;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_place", joinColumns = {
+            @JoinColumn(name = "place_id") }, inverseJoinColumns = { @JoinColumn(name = "user_id") })
+    private List<User> users;
+
     public Place(GeolocationResponse geolocationResponse) {
         this.latitude = geolocationResponse.getLatitude();
         this.longitude = geolocationResponse.getLongitude();
         this.address = geolocationResponse.getFormattedAddress();
         this.api_placeId = geolocationResponse.getPlaceId();
+        this.ratings = new ArrayList<>();
+        this.comments = new ArrayList<>();
+        this.totalRating = 0.0;
+        this.users = new ArrayList<>();
     }
 
     public void addComment(Comment comment) {
