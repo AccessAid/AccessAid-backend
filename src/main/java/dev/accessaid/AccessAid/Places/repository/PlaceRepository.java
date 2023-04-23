423191f36f398b64ce28fa6adea7563eb412c97d
Vero Conesa <vero.gconesa@gmail.com>
2023-04-23 14:58:42 +0200

    Created PlaceRepository file

---
+++ src/main/java/dev/accessaid/AccessAid/Places/repository/PlaceRepository.java
@@ -0,0 +1,13 @@
+package dev.accessaid.AccessAid.Places.repository;
+
+import org.springframework.stereotype.Repository;
+
+import dev.accessaid.AccessAid.Places.model.Place;
+
+@Repository
+public class PlaceRepository {
+
+    public void save(Place newPlace) {
+    }
+
+}
