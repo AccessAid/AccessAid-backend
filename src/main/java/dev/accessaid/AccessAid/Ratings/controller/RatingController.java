be8bc0a051a0e075cbff027e5f3ea62a5ecc7b52
Vero Conesa <vero.gconesa@gmail.com>
2023-04-23 14:58:17 +0200

    Added info for Swagger

--- src/main/java/dev/accessaid/AccessAid/Ratings/controller/RatingController.java
+++ src/main/java/dev/accessaid/AccessAid/Ratings/controller/RatingController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Ratings", description = "Ratings of the places")
 @RestController
 public class RatingController {
 
