ebf8dca8cbc33b434c17230df3a707981ed55d0e
Vero Conesa <vero.gconesa@gmail.com>
2023-04-24 13:14:24 +0200

    Added methods to placeRepository

--- src/main/java/dev/accessaid/AccessAid/Places/repository/PlaceRepository.java
+++ src/main/java/dev/accessaid/AccessAid/Places/repository/PlaceRepository.java
@@ -3,25 +3,27 @@ package dev.accessaid.AccessAid.Places.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dev.accessaid.AccessAid.Comments.model.Comment;
 import dev.accessaid.AccessAid.Places.model.Place;
+import dev.accessaid.AccessAid.Ratings.model.Rating;
 import dev.accessaid.AccessAid.model.User;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Integer> {
 
-    // List<Place> findAllPlaces();
+    List<User> findUsersById(Integer placeId);
 
-    // Place findPlaceById(Integer id);
+    List<Place> findByUserId(Integer userId);
 
-    // <S extends Place> S save(S place);
+    List<Comment> findCommentsById(Integer placeId);
 
-    // Place deletePlaceById(Integer id);
+    @Query(value = "SELECT AVG(r.rating) FROM Place p JOIN p.ratings r WHERE p.id = ?1")
+    Double findTotalRatingByPlace(Integer placeId);
 
-    List<User> findUsersByid(Integer id);
-
-    List<Comment> findCommentsByid(Integer id);
+    @Query(value = "SELECT r FROM Place p JOIN p.ratings r WHERE p.id = ?1")
+    List<Rating> findAllRatingsByPlace(Integer placeId);
 
 }
