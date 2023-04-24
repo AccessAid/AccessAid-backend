6f72d998c89e96e3386f99cff667719a9dc67f7d
Vero Conesa <vero.gconesa@gmail.com>
2023-04-24 19:17:10 +0200

    Updated basic queries

--- src/main/java/dev/accessaid/AccessAid/Comments/repository/CommentRepository.java
+++ src/main/java/dev/accessaid/AccessAid/Comments/repository/CommentRepository.java
@@ -3,7 +3,6 @@ package dev.accessaid.AccessAid.Comments.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dev.accessaid.AccessAid.Comments.model.Comment;
@@ -12,15 +11,6 @@ import dev.accessaid.AccessAid.model.User;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-
-    @Query(value = "SELECT * FROM comment", nativeQuery = true)
-    List<Comment> findAllComments();
-
-    Comment findCommentById(Integer id);
-
-    void deleteCommentById(Integer id);
-
-    <S extends Comment> S save(S comment);
 
     List<Comment> findAllCommentsByPlace(Place place);
 
