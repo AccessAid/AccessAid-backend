0e6b8cb88eb80b21e323886dc418599a7f6cc0de
Cristobal Rosa <crisrogar@gmail.com>
2023-04-24 17:56:09 +0200

    feat:Creacion clase JwtResponse

--- src/main/java/dev/accessaid/AccessAid/AccessAidApplication.java
+++ src/main/java/dev/accessaid/AccessAid/AccessAidApplication.java
@@ -2,8 +2,6 @@ package dev.accessaid.AccessAid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AccessAidApplication {
