7f751d47120713ff20f8f895f2a6b657aef330f7
Vero Conesa <vero.gconesa@gmail.com>
2023-04-26 00:01:03 +0200

    Created UserController

--- src/main/java/dev/accessaid/AccessAid/User/controller/UserController.java
+++ src/main/java/dev/accessaid/AccessAid/User/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.accessaid.AccessAid.Profile.exceptions.ProfileNotFoundException;
 import dev.accessaid.AccessAid.Profile.model.Profile;
 import dev.accessaid.AccessAid.Profile.service.ProfileServiceImpl;
 import dev.accessaid.AccessAid.User.exceptions.UserNotFoundException;
@@ -158,4 +159,108 @@ public class UserController {
 
     }
 
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        try {
+            User userToUpdate = userService.getUserById(user.getId());
+            if (userToUpdate == null) {
+                throw new UserNotFoundException("User not found");
+            }
+            User updatedUser = userService.changeUser(user);
+            UserResponse response = UserMapper.toUserResponse(updatedUser);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Integer id) {
+        try {
+            User userToDelete = userService.getUserById(id);
+            if (userToDelete == null) {
+                throw new UserNotFoundException("User not found");
+            }
+            userService.removeUser(id);
+            UserResponse response = UserMapper.toUserResponse(userToDelete);
+            return ResponseEntity.ok(response);
+        } catch (ProfileNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+
+    }
+
+    @GetMapping("/profile/{profileId}")
+    public ResponseEntity<?> seeUserByProfile(@PathVariable Integer profileId) {
+        try {
+            Profile profile = profileService.getProfileById(profileId);
+            if (profile == null) {
+                ErrorResponse errorResponse = new ErrorResponse("Profile not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            User user = userService.getUserByProfile(profile);
+            if (user == null) {
+                ErrorResponse errorResponse = new ErrorResponse("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<?> seeUserByEmail(@PathVariable String email) {
+        try {
+            User user = userService.getUserByEmail(email);
+            if (user == null) {
+                ErrorResponse errorResponse = new ErrorResponse("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<?> seeUserByUsername(@PathVariable String username) {
+        try {
+            User user = userService.getUserByUsername(username);
+            if (user == null) {
+                ErrorResponse errorResponse = new ErrorResponse("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
 }
