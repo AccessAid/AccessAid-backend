c34ff3904867da1477f5886a928571fad2f6245c
Cristobal Rosa <crisrogar@gmail.com>
2023-04-25 12:59:41 +0200

    feat:crear clase UserDetailsServiceImpl

--- src/main/java/dev/accessaid/AccessAid/repository/UserRepository.java
+++ src/main/java/dev/accessaid/AccessAid/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package dev.accessaid.AccessAid.repository;
 
 import dev.accessaid.AccessAid.model.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
+    Optional<User> findByUsername(String username);
 }
