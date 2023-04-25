16808402602d30f789fc4936145d2a1c6f01fe51
Vero Conesa <vero.gconesa@gmail.com>
2023-04-26 00:01:03 +0200

    Created UserController

--- src/main/java/dev/accessaid/AccessAid/User/controller/UserController.java
+++ src/main/java/dev/accessaid/AccessAid/User/controller/UserController.java
@@ -3,31 +3,187 @@ package dev.accessaid.AccessAid.User.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.accessaid.AccessAid.Geolocation.Response.ErrorResponse;
+import dev.accessaid.AccessAid.Profile.exceptions.ProfileNotFoundException;
+import dev.accessaid.AccessAid.Profile.model.Profile;
+import dev.accessaid.AccessAid.Profile.service.ProfileServiceImpl;
+import dev.accessaid.AccessAid.User.exceptions.UserNotFoundException;
+import dev.accessaid.AccessAid.User.exceptions.UserSaveException;
 import dev.accessaid.AccessAid.User.model.User;
-import dev.accessaid.AccessAid.User.service.UserService;
+import dev.accessaid.AccessAid.User.response.UserResponse;
+import dev.accessaid.AccessAid.User.service.UserServiceImpl;
+import dev.accessaid.AccessAid.User.utils.UserMapper;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Users", description = "Information about Users")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userService;
+
+    @Autowired
+    private ProfileServiceImpl profileService;
 
     @GetMapping("")
-    public List<User> seeAllUsers() {
-        return userService.getUsers();
+    public ResponseEntity<?> seeAllUsers() {
+        try {
+            List<User> users = userService.getUsers();
+            List<UserResponse> response = UserMapper.toUserResponses(users);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> seeUserById(@PathVariable Integer id) {
+        try {
+            User user = userService.getUserById(id);
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+
     }
 
     @PostMapping("")
-    public User addUser(@RequestBody User user) {
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        try {
+            User newUser = userService.createUser(user);
+            UserResponse response = UserMapper.toUserResponse(newUser);
+            return ResponseEntity.ok(response);
+        } catch (UserSaveException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 
-        return userService.createUser(user);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        try {
+            User userToUpdate = userService.getUserById(user.getId());
+            if (userToUpdate == null) {
+                throw new UserNotFoundException("User not found");
+            }
+            User updatedUser = userService.changeUser(user);
+            UserResponse response = UserMapper.toUserResponse(updatedUser);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Integer id) {
+        try {
+            User userToDelete = userService.getUserById(id);
+            if (userToDelete == null) {
+                throw new UserNotFoundException("User not found");
+            }
+            userService.removeUser(id);
+            UserResponse response = UserMapper.toUserResponse(userToDelete);
+            return ResponseEntity.ok(response);
+        } catch (ProfileNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+
+    }
+
+    @GetMapping("/profile/{profileId}")
+    public ResponseEntity<?> seeUserByProfile(@PathVariable Integer profileId) {
+        try {
+            Profile profile = profileService.getProfileById(profileId);
+            if (profile == null) {
+                ErrorResponse errorResponse = new ErrorResponse("Profile not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            User user = userService.getUserByProfile(profile);
+            if (user == null) {
+                ErrorResponse errorResponse = new ErrorResponse("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<?> seeUserByEmail(@PathVariable String email) {
+        try {
+            User user = userService.getUserByEmail(email);
+            if (user == null) {
+                ErrorResponse errorResponse = new ErrorResponse("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<?> seeUserByUsername(@PathVariable String username) {
+        try {
+            User user = userService.getUserByUsername(username);
+            if (user == null) {
+                ErrorResponse errorResponse = new ErrorResponse("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+            }
+            UserResponse response = UserMapper.toUserResponse(user);
+            return ResponseEntity.ok(response);
+        } catch (UserNotFoundException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        } catch (Exception e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
 }
