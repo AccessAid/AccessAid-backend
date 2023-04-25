fafe69625d24ba2bb721d99a1d24bd3763211837
Cristobal Rosa <crisrogar@gmail.com>
2023-04-25 13:17:36 +0200

    feat:crear clase JwtRequestFilter

--- src/main/java/dev/accessaid/AccessAid/security/config/SecurityConfig.java
+++ src/main/java/dev/accessaid/AccessAid/security/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package dev.accessaid.AccessAid.security.config;
 
 import dev.accessaid.AccessAid.security.jwt.JwtAuthEntryPoint;
+import dev.accessaid.AccessAid.security.jwt.JwtRequestFilter;
+import dev.accessaid.AccessAid.security.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +19,21 @@ public class SecurityConfig {
 
     @Bean JwtAuthEntryPoint unauthorizedHandler() {
         return new JwtAuthEntryPoint();
+    }
+
+    @Bean
+    public JwtRequestFilter authenticationJwtTokenFilter() {
+        return new JwtRequestFilter();
+    }
+
+    @Bean
+    public UserDetailsServiceImpl userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
