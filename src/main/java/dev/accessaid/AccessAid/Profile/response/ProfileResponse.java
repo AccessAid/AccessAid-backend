663cb7c75cd1b927f49420f576e177558375e282
Vero Conesa <vero.gconesa@gmail.com>
2023-04-25 22:28:36 +0200

    Created ProfileResponse

---
+++ src/main/java/dev/accessaid/AccessAid/Profile/response/ProfileResponse.java
@@ -0,0 +1,32 @@
+package dev.accessaid.AccessAid.Profile.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileResponse {
+
+    @JsonProperty("id")
+    private Integer id;
+
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("avatarPath")
+    private String avatarPath;
+
+}
