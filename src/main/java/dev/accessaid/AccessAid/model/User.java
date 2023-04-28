4748b8c42581f7f0bdd2840062bd31bffc22cce3
Verónica Conesa <90536006+Othamae@users.noreply.github.com>
2023-04-28 14:57:38 +0200

    Changed Long to Integer

--- src/main/java/dev/accessaid/AccessAid/model/User.java
+++ src/main/java/dev/accessaid/AccessAid/model/User.java
@@ -23,7 +23,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String username;
     private String email;
     private String password;
