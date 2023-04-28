0e1a28ba90fb79531dc457f47021d2cbae7bad16
Verónica Conesa <90536006+Othamae@users.noreply.github.com>
2023-04-28 15:42:43 +0200

    Update AccessAidApplication.java

--- src/main/java/dev/accessaid/AccessAid/AccessAidApplication.java
+++ src/main/java/dev/accessaid/AccessAid/AccessAidApplication.java
@@ -1,12 +1,7 @@
 package dev.accessaid.AccessAid;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccessAidApplication {
