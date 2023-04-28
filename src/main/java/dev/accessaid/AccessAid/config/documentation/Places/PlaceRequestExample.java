322ef95a35807ead2cda0c6f7874582777f2a212
Vero Conesa <vero.gconesa@gmail.com>
2023-04-29 00:57:32 +0200

    Added PlaceRequestExample

---
+++ src/main/java/dev/accessaid/AccessAid/config/documentation/Places/PlaceRequestExample.java
@@ -0,0 +1,18 @@
+package dev.accessaid.AccessAid.config.documentation.Places;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class PlaceRequestExample {
+
+    @Schema(description = "Address of the place", example = ExamplesValues.ADDRESS)
+    private String address;
+
+    @Schema(description = "Latitude of the place", example = ExamplesValues.LATITUDE)
+    private Double latitude;
+
+    @Schema(description = "Longitude of the place", example = ExamplesValues.LONGITUDE)
+    private Double longitude;
+
+}
