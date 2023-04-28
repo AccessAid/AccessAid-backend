628cd888d2ad23cb3ef11a7f1fd308c097909c04
Vero Conesa <vero.gconesa@gmail.com>
2023-04-29 00:53:52 +0200

    Added Swagger

---
+++ src/main/java/dev/accessaid/AccessAid/config/documentation/Places/ExamplesValues.java
@@ -0,0 +1,13 @@
+package dev.accessaid.AccessAid.config.documentation.Places;
+
+public class ExamplesValues {
+
+    public static final String LATITUDE = "42.0339357";
+
+    public static final String LONGITUDE = "-87.6721867";
+
+    public static final String ADDRESS = "123 Main St, Evanston, IL 60202, USA";
+
+    public static final String API_PLACE_ID = "ChIJDYp7EELQD4gRDzMUL_0DFlU";
+
+}
