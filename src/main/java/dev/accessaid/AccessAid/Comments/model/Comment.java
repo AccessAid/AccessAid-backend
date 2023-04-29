2c093aa59ff83497bb8f74f8c25826a79321763d
Vero Conesa <vero.gconesa@gmail.com>
2023-04-29 13:46:28 +0200

    Added Swagger

--- src/main/java/dev/accessaid/AccessAid/Comments/model/Comment.java
+++ src/main/java/dev/accessaid/AccessAid/Comments/model/Comment.java
@@ -2,6 +2,7 @@ package dev.accessaid.AccessAid.Comments.model;
 
 import dev.accessaid.AccessAid.Places.model.Place;
 import dev.accessaid.AccessAid.User.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,9 +24,13 @@ import lombok.NoArgsConstructor;
 @Table(name = "comment")
 public class Comment {
 
+    @Schema(example = "1", description = "")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @Schema(description = "Comment", example = "Nice place!")
+    @NotNull
     String comment;
 
     @ManyToOne
