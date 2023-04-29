fdce7692b9dcf1f9007b63ec1949cfebd4e6c73f
Vero Conesa <vero.gconesa@gmail.com>
2023-04-29 13:53:45 +0200

    Added commentResponseExample

--- src/main/java/dev/accessaid/AccessAid/config/documentation/Comments/CommentResponseExample.java
+++ src/main/java/dev/accessaid/AccessAid/config/documentation/Comments/CommentResponseExample.java
@@ -1,5 +1,6 @@
 package dev.accessaid.AccessAid.config.documentation.Comments;
 
+import dev.accessaid.AccessAid.config.documentation.ExamplesValues;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -9,7 +10,7 @@ public class CommentResponseExample {
     @Schema(description = "Unique ID of the comment", example = "1")
     private Integer id;
 
-    @Schema(description = "Comment", example = "This place has good accessibility for wheelchair users.")
+    @Schema(description = "Comment from a user", example = ExamplesValues.COMMENT)
     private String comment;
 
     @Schema(description = "ID of user who made the comment", example = "1")
