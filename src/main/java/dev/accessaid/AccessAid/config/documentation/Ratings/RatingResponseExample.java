0506060d878045aa1629cfa9337fc791d78e5972
Vero Conesa <vero.gconesa@gmail.com>
2023-04-29 11:59:28 +0200

    Updated RatingResponseExample

--- src/main/java/dev/accessaid/AccessAid/config/documentation/Ratings/RatingResponseExample.java
+++ src/main/java/dev/accessaid/AccessAid/config/documentation/Ratings/RatingResponseExample.java
@@ -9,7 +9,7 @@ public class RatingResponseExample {
     @Schema(description = "Unique ID of the rating", example = "1")
     private Integer id;
 
-    @Schema(description = "Rating value", example = "5")
+    @Schema(description = "Rating value", example = "4.5")
     private double rating;
 
     @Schema(description = "User ID", example = "1")
