5051f4034c100b706865d14609b26e9788257765
Vero Conesa <vero.gconesa@gmail.com>
2023-04-29 19:36:33 +0200

    Added Swagger

--- src/main/java/dev/accessaid/AccessAid/Profile/model/Profile.java
+++ src/main/java/dev/accessaid/AccessAid/Profile/model/Profile.java
@@ -1,6 +1,8 @@
 package dev.accessaid.AccessAid.Profile.model;
 
 import dev.accessaid.AccessAid.User.model.User;
+import dev.accessaid.AccessAid.config.documentation.ExamplesValues;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,12 +24,18 @@ import lombok.NoArgsConstructor;
 @Table(name = "profile", uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id" }) })
 public class Profile {
 
+    @Schema(example = "1", description = "")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
 
+    @Schema(example = ExamplesValues.FIRSTNAME, description = "")
     String firstName;
+
+    @Schema(example = ExamplesValues.LASTNAME, description = "")
     String lastName;
+
+    @Schema(example = ExamplesValues.AVATAR_PATH, description = "")
     String avatarPath;
 
     @OneToOne
