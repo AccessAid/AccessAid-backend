343e4601a142814ac4711c698d239180bf25691a
Vero Conesa <vero.gconesa@gmail.com>
2023-05-04 22:11:51 +0200

    Deleted @Repository as is not needed

--- src/main/java/dev/accessaid/AccessAid/Ratings/repository/RatingRepository.java
+++ src/main/java/dev/accessaid/AccessAid/Ratings/repository/RatingRepository.java
@@ -3,13 +3,11 @@ package dev.accessaid.AccessAid.Ratings.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import dev.accessaid.AccessAid.Places.model.Place;
 import dev.accessaid.AccessAid.Ratings.model.Rating;
 import dev.accessaid.AccessAid.User.model.User;
 
-@Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
     List<Rating> findByPlace(Place place);
--- src/main/java/dev/accessaid/AccessAid/User/repository/UserRepository.java
+++ src/main/java/dev/accessaid/AccessAid/User/repository/UserRepository.java
@@ -3,12 +3,10 @@ package dev.accessaid.AccessAid.User.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import dev.accessaid.AccessAid.Profile.model.Profile;
 import dev.accessaid.AccessAid.User.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Boolean existsByUsername(String username);
