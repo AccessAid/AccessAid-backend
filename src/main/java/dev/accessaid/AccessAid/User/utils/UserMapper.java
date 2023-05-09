a9bcca84d04b75a2563d3e04609b9efb6a50f4c5
Vero Conesa <vero.gconesa@gmail.com>
2023-05-09 23:40:38 +0200

    Added pagination

--- src/main/java/dev/accessaid/AccessAid/User/utils/UserMapper.java
+++ src/main/java/dev/accessaid/AccessAid/User/utils/UserMapper.java
@@ -3,6 +3,10 @@ package dev.accessaid.AccessAid.User.utils;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
 import dev.accessaid.AccessAid.User.model.User;
 import dev.accessaid.AccessAid.User.response.UserResponse;
 
@@ -19,4 +23,8 @@ public class UserMapper {
         return users.stream().map(UserMapper::toUserResponse).collect(Collectors.toList());
     }
 
+    public static Page<UserResponse> toUserResponses(Page<User> users, Pageable pageable) {
+        List<UserResponse> userResponses = users.stream().map(UserMapper::toUserResponse).collect(Collectors.toList());
+        return new PageImpl<>(userResponses, pageable, users.getTotalElements());
+    }
 }
