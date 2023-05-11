1f875011824675fe5613d18c997512cd9c3bbbdb
Cristobal Rosa <crisrogar@gmail.com>
2023-05-11 17:55:44 +0200

    feat: RatingSaveException

--- src/main/java/dev/accessaid/AccessAid/Ratings/exceptions/RatingSaveException.java
+++ src/main/java/dev/accessaid/AccessAid/Ratings/exceptions/RatingSaveException.java
@@ -1,10 +1,15 @@
 package dev.accessaid.AccessAid.Ratings.exceptions;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class RatingSaveException extends RuntimeException {
 
     private String message;
