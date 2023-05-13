47c2a650c4d58dfb450beb49bad448258cf6eeb0
Vero Conesa <vero.gconesa@gmail.com>
2023-05-14 00:45:32 +0200

    Added MailConfig

--- src/main/java/dev/accessaid/AccessAid/config/OpenAPIConfig.java
+++ src/main/java/dev/accessaid/AccessAid/config/OpenAPIConfig.java
@@ -13,7 +13,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
-@Import(PageableConfig.class)
+@Import({ MailConfig.class, PageableConfig.class })
 public class OpenAPIConfig {
 
         @Value("http://localhost:8080")
