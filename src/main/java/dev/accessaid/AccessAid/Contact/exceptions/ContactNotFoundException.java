89dcaae0c5dba96b560a0165397d007cccc89e9d
Vero Conesa <vero.gconesa@gmail.com>
2023-05-14 21:40:25 +0200

    Added contactNotFoundException

--- src/main/java/dev/accessaid/AccessAid/Contact/exceptions/ContactNotFoundException.java
+++ src/main/java/dev/accessaid/AccessAid/Contact/exceptions/ContactNotFoundException.java
@@ -1,10 +1,16 @@
 package dev.accessaid.AccessAid.Contact.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ContactNotFoundException extends RuntimeException {
 
     private String message;
