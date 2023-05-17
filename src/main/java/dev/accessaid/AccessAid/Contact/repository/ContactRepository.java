9c26460660d2b42a1dec725d49d1cdf1d09891b8
Vero Conesa <vero.gconesa@gmail.com>
2023-05-17 21:33:51 +0200

    Deleted unnecessary anotation

--- src/main/java/dev/accessaid/AccessAid/Contact/repository/ContactRepository.java
+++ src/main/java/dev/accessaid/AccessAid/Contact/repository/ContactRepository.java
@@ -3,11 +3,9 @@ package dev.accessaid.AccessAid.Contact.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import dev.accessaid.AccessAid.Contact.model.Contact;
 
-@Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
     Optional<Contact> findByEmail(String email);
