7334b1c7ca2429e3d6a853b7cc662f9d3b27feee
Verónica Conesa <90536006+Othamae@users.noreply.github.com>
2023-05-17 20:42:51 +0200

    Update UserRepository.java
    Deleted import of Profile model, as it is not needed anymore

--- src/main/java/dev/accessaid/AccessAid/User/repository/UserRepository.java
+++ src/main/java/dev/accessaid/AccessAid/User/repository/UserRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import dev.accessaid.AccessAid.Profile.model.Profile;
 import dev.accessaid.AccessAid.User.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
