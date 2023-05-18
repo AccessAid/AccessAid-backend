2eddd1e42235937c3da1a5f16a8b46364b5e777e
Vero Conesa <vero.gconesa@gmail.com>
2023-05-18 13:59:55 +0200

    Added Service for getNearbyPlaces

--- src/main/java/dev/accessaid/AccessAid/Accessibility/service/AccessibilityService.java
+++ src/main/java/dev/accessaid/AccessAid/Accessibility/service/AccessibilityService.java
@@ -1,8 +1,14 @@
 package dev.accessaid.AccessAid.Accessibility.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.maps.GeoApiContext;
 import com.google.maps.PlacesApi;
 import com.google.maps.model.AddressType;
@@ -10,9 +16,22 @@ import com.google.maps.model.Photo;
 import com.google.maps.model.PlaceDetails;
 
 import dev.accessaid.AccessAid.Accessibility.response.AccessibilityResponse;
+import dev.accessaid.AccessAid.Accessibility.response.NearbyPlacesResponse;
+import dev.accessaid.AccessAid.Accessibility.response.PlaceAndNearbyPlacesResponse;
+import dev.accessaid.AccessAid.Geolocation.Response.GeolocationResponse;
+import dev.accessaid.AccessAid.Places.service.GeolocationUtils;
+import dev.accessaid.AccessAid.Places.utils.PlaceRequest;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 @Service
 public class AccessibilityService {
+
+    @Value("${spring.sendgrid.api-key}")
+    String apiKey;
+
+    @Autowired
+    private GeolocationUtils geolocationUtils;
 
     private final GeoApiContext context;
 
@@ -23,6 +42,7 @@ public class AccessibilityService {
     }
 
     public AccessibilityResponse getPlaceDetails(String placeId) throws Exception {
+
         PlaceDetails details = PlacesApi.placeDetails(context, placeId).await();
 
         String name = details.name != null ? details.name : null;
@@ -35,10 +55,50 @@ public class AccessibilityService {
         AddressType[] types = details.types != null ? details.types : null;
         Photo[] photos = details.photos != null ? details.photos : null;
 
-        AccessibilityResponse response = new AccessibilityResponse(name, phone, url, wheelchair_accessible_entrance,
+        AccessibilityResponse response = new AccessibilityResponse(name, phone, url,
+                wheelchair_accessible_entrance,
                 website,
                 types, photos);
         return response;
+    }
+
+    public PlaceAndNearbyPlacesResponse getNearbyPlaces(Double lat, Double lng, String address, String type)
+            throws Exception {
+
+        PlaceRequest placeRequest = new PlaceRequest(address, lat, lng);
+
+        GeolocationResponse geolocationResponse = geolocationUtils
+                .getGeolocationByAddressOrCoordinates(placeRequest);
+
+        double latitud = geolocationResponse.getLatitude();
+        double longitude = geolocationResponse.getLongitude();
+
+        String addressType = type == null ? "" : type.toLowerCase();
+        Response response = NearbyPlaceUtils.getResponse(latitud, longitude, addressType, apiKey);
+
+        AccessibilityResponse accessibilityResponse = getPlaceDetails(geolocationResponse.getPlaceId());
+
+        ResponseBody responseBody = response.body();
+        String json = responseBody != null ? responseBody.string() : "";
+
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        JsonElement results = jsonObject.get("results");
+        JsonElement status = jsonObject.get("status");
+        if (!status.getAsString().equals("OK")) {
+            throw new Exception(status.getAsString());
+
+        }
+
+        String next_page_token = jsonObject.get("next_page_token") != null
+                ? jsonObject.get("next_page_token").getAsString()
+                : null;
+
+        List<NearbyPlacesResponse> nearbyPlaces = NearbyPlaceUtils.getNearbyPlaces(results);
+
+        PlaceAndNearbyPlacesResponse placeAndNearbyPlacesResponse = new PlaceAndNearbyPlacesResponse(
+                accessibilityResponse, nearbyPlaces, next_page_token);
+
+        return placeAndNearbyPlacesResponse;
     }
 
 }
