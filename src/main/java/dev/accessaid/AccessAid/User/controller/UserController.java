f0a4a2f049f814965cca61a8575428eb7cbec9fe
Verónica Conesa <90536006+Othamae@users.noreply.github.com>
2023-05-20 12:20:38 +0200

    Update UserController.java
    Deleted unneccesary import

--- src/main/java/dev/accessaid/AccessAid/User/controller/UserController.java
+++ src/main/java/dev/accessaid/AccessAid/User/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
