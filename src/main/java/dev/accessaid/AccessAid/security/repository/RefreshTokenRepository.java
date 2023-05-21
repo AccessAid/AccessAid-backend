644ee2af82b875e67e5c3e7033df4000993734a5
Cristobal Rosa <crisrogar@gmail.com>
2023-05-21 13:28:16 +0200

    feat: repository RefreshTokenRepository

---
+++ src/main/java/dev/accessaid/AccessAid/security/repository/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package dev.accessaid.AccessAid.security.repository;
+
+import dev.accessaid.AccessAid.User.model.User;
+import dev.accessaid.AccessAid.security.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    int deleteByUser(User user);
+}
