67efdf0bec6796c5b0770ac87d1147bad0741939
Cristobal Rosa <crisrogar@gmail.com>
2023-05-21 13:27:23 +0200

    feat: model RefreshToken

---
+++ src/main/java/dev/accessaid/AccessAid/security/model/RefreshToken.java
@@ -0,0 +1,32 @@
+package dev.accessaid.AccessAid.security.model;
+
+import dev.accessaid.AccessAid.User.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "refreshtoken")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
