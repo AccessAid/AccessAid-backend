0800e9b6b4833a5d24ad46a29d33d884aeb2b35d
Vero Conesa <vero.gconesa@gmail.com>
2023-05-23 11:12:23 +0200

    Added POST contact to open routes, as it should be open to anyone

--- src/main/java/dev/accessaid/AccessAid/security/config/SecurityConfig.java
+++ src/main/java/dev/accessaid/AccessAid/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -62,6 +63,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/contact").permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults());
 
